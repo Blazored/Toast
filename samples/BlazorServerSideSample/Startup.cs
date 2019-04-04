d4302f4d423dee6ae1151e57b7f852ce7a3cae08
Lars Glud <lg@danrobotics.dk>
2019-04-04 16:31:33 +0200

    Added Serverside Sample

---
+++ samples/BlazorServerSideSample/Startup.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using BlazorServerSideSample.Components;
+using Blazored.Toast;
+using Blazored.Toast.Configuration;
+
+namespace BlazorServerSideSample
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddNewtonsoftJson();
+
+            services.AddRazorComponents();
+
+            services.AddBlazoredToast(options => {
+                options.Timeout = 10; // default: 5
+                options.Position = ToastPosition.BottomRight; // default: ToastPosition.TopRight
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting(routes =>
+            {
+                routes.MapRazorPages();
+                routes.MapComponentHub<App>("app");
+            });
+        }
+    }
+}
