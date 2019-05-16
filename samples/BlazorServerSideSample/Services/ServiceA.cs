587fd77f275bcc2efbcefe3899bb9860ab69ac98
Chris Sainty <chris.sainty@flagship-group.co.uk>
2019-05-16 09:27:32 +0100

    Fixed positioning bug and upgraded to preview 5

---
+++ samples/BlazorServerSideSample/Services/ServiceA.cs
@@ -0,0 +1,18 @@
+﻿using Blazored.Toast.Services;
+
+namespace BlazorServerSideSample.Services
+{
+    public class ServiceA
+    {
+        private readonly IToastService _toastService;
+
+        public ServiceA(IToastService toastService)
+        {
+            _toastService = toastService;
+        }
+        public void ShowMessage()
+        {
+            _toastService.ShowSuccess("I'm a SUCCESS message with a custom title", "Congratulations!");
+        }
+    }
+}
